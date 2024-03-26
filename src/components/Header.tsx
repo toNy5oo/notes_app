3dcb725373bfab17194fa0a194b5d7f513495897
toNy5oo <lindirizzoemail@gmail.com>
2024-03-26 08:47:09 +0100

    Add toggleLayout button to change from grid to list

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,19 @@
 import { INotes } from '@/interface/notes_interface';
 import { NoteForm } from './NoteForm';
+import { LAYOUT } from '@/const/enums';
+import { Button } from './ui/button';
+import { CardIcon, ListIcon } from './ui/icones';
 
 interface Props {
   notes: INotes;
   setNotes: React.Dispatch<React.SetStateAction<INotes>>;
+  toggleLayout: React.Dispatch<LAYOUT>;
+  layout: string;
 }
 
-export default function Header({notes, setNotes}: Props) {
+export default function Header({notes, setNotes, toggleLayout, layout}: Props) {
+
+  console.log(layout)
 
     function createNewNote (newNote: INote) {
         setNotes([...notes, newNote])
@@ -15,6 +22,11 @@ export default function Header({notes, setNotes}: Props) {
   return (
     <div className="flex justify-between mb-4 p-4 border-b-2">
         <NoteForm createNewNote={createNewNote}/>
+        <Button variant="outline" size="icon" onClick={(e) => toggleLayout(e)}>
+      { 
+      layout === 'card' ? (<CardIcon className="h-4 w-4" />) : (<ListIcon className="h-4 w-4" />)
+    }
+    </Button>
     </div>
   )
 }
