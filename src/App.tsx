d737d2be581273f2782a90f0ed0418f461c4d911
toNy5oo <lindirizzoemail@gmail.com>
2024-03-26 08:46:31 +0100

    Add layoutand deleteNote function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import './App.css'
 import Header from './components/Header'
-import Notes from './components/Notes'
-import { useState } from 'react'
+import Note from './components/Note'
+import { useReducer, useState } from 'react'
 import { notes_array } from './const/notes_array'
 import { INote } from './interface/notes_interface'
 
-
 function App() {
-  const [notes , setNotes] = useState<INote[]>(notes_array)
 
-  //Fetch Notes from DB in state, display them in 3 col cards
-
+  const [layout, toggleLayout] = useReducer((state) => state === 'card' ? 'list' : 'card', 'card');
+  
+  const [notes, setNotes] = useState<INote[]>(notes_array);
+  
+  function deleteNote(id: number): void {
+    const filteredArray = notes.filter((note) => (note.id!== id));
+    setNotes(filteredArray);
+  }
+  
   return (
     <>
-      <Header notes={notes} setNotes={setNotes} />
-      <div className='flex gap-4 flex-wrap justify-evenly'>
-      {
-        notes.map(note => (
-          <Notes key={note.id} note={note} />
-        ))
-      }
+      <Header notes={notes} setNotes={setNotes} toggleLayout={toggleLayout} layout={layout}/>
+      <div className={`flex gap-4 flex-wrap justify-evenly ${layout === "list" && "flex-col" }`}>
+        {
+          notes.map(note => (
+            <Note key={note.id} note={note} deleteNote={deleteNote} layout={layout}/>
+          ))
+        }
       </div>
     </>
   )
