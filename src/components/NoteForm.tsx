fafaa4e0de257a706cd2b0b41005e3d652640c30
toNy5oo <lindirizzoemail@gmail.com>
2024-03-28 11:38:07 +0100

    Dialog instead Popover
    Use a dialog instead of a popover to create a note

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -2,54 +2,78 @@ import { Input } from "./ui/input"
 import { Textarea } from "./ui/textarea"
 import { useState } from "react"
 import { INote } from "@/interface/notes_interface"
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { AddCircleRounded } from "./ui/icones";
+
+
 
 interface Props {
   createNewNote: React.Dispatch<React.SetStateAction<INote>>;
 }
 
 export function NoteForm({createNewNote}: Props): JSX.Element {
+
   const [note, setNote] = useState<INote>({} as INote)
 
+
   return (
-    <Popover>
-      <PopoverTrigger>
-        <Button variant="destructive">New note</Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-80">
-        <div className="grid gap-4">
-          <div className="space-y-2">
-            <h4 className="font-medium leading-none">Create new Note</h4>
-            <p className="text-sm text-muted-foreground">
-              Add the details of the note to add
-            </p>
-          </div>
-          <div className="grid gap-2">
-            <div className="grid grid-cols-3 items-center gap-4">
-              <Label htmlFor="note_title">Title</Label>
-              <Input
-                id="note_title"
-                className="col-span-2 h-8"
-                onChange={(e) => {
-                  setNote({...note, title: e.target.value})}} 
-              />
-            </div>
-            <div className="grid grid-cols-3 items-center gap-4">
-              <Label htmlFor="note_description">Description</Label>
-              <Textarea
-              id="note_description" 
-              maxLength={300}
-              placeholder="Type here..." 
+    <>
+      <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="default"><AddCircleRounded className="mr-2 h-4 w-4" />New Note</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create new note</DialogTitle>
+          <DialogDescription>
+          Add the details of the note to add.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="note_title" className="text-right">
+              Title
+            </Label>
+            <Input
+              id="note_title"
               className="col-span-2 h-8"
               onChange={(e) => {
-                setNote({...note, description: e.target.value})}}/>
-            </div>
+                setNote({...note, title: e.target.value})}} 
+            />
           </div>
-          <Button variant="destructive" onClick={() => createNewNote(note)}>Create</Button>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="note_description" className="text-right">
+              Description
+            </Label>
+            <Textarea
+            id="note_description" 
+            maxLength={300}
+            placeholder="Type here..." 
+            className="col-span-2 h-8"
+            onChange={(e) => {
+              setNote({...note, description: e.target.value})}}/>
+          </div>
         </div>
-      </PopoverContent>
-    </Popover>
+        <DialogFooter>
+        <DialogClose asChild>
+        <Button variant="destructive" onClick={() => createNewNote(note)}>Create</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+    
+    </>
   )
+  
 }
