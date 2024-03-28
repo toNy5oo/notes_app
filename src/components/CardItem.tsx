d585a90d1e36a816bbf324ed7175268499b807c4
toNy5oo <lindirizzoemail@gmail.com>
2024-03-28 11:44:54 +0100

    Change UI layout for Note Card

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -3,16 +3,18 @@ import { Button } from "./ui/button";
 import {  Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { DeleteBin } from "./ui/icones";
 
+
 interface Props {
     note: INote;
     deleteNote: (string: number) => void;
   }
 
 export default function CardItem({note, deleteNote}: Props) {
+
   return (
-    <Card className="w-[350px] bg-slate-50">
-      <CardHeader>
-        <CardTitle>{note.title}</CardTitle>
+    <Card className="w-[280px] bg-slate-50 text-start">
+      <CardHeader className="flex flex-row justify-between items-center">
+        <CardTitle className="text-xl">{note.title}</CardTitle>
         <Button variant="ghost" onClick={() => deleteNote(note.id)}><DeleteBin className="text-lg text-red-600"/></Button>
       </CardHeader>
       <CardContent>
