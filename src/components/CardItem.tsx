66dfbcad901f884f1fc1cec5c4e80f6cbed9d25e
toNy5oo <lindirizzoemail@gmail.com>
2024-03-28 11:43:49 +0100

    Change UI of button to delete

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,6 +1,7 @@
 import { INote } from "@/interface/notes_interface";
 import { Button } from "./ui/button";
 import {  Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { DeleteBin } from "./ui/icones";
 
 interface Props {
     note: INote;
@@ -12,12 +13,12 @@ export default function CardItem({note, deleteNote}: Props) {
     <Card className="w-[350px] bg-slate-50">
       <CardHeader>
         <CardTitle>{note.title}</CardTitle>
+        <Button variant="ghost" onClick={() => deleteNote(note.id)}><DeleteBin className="text-lg text-red-600"/></Button>
       </CardHeader>
       <CardContent>
       {note.description}
       </CardContent>
       <CardFooter className="flex justify-between">
-      <Button onClick={() => deleteNote(note.id)}>Delete</Button>
       </CardFooter>
     </Card>
   )
