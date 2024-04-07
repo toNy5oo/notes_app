d0707b7269754b48db174fd303a824f293a5ca22
toNy5oo <lindirizzoemail@gmail.com>
2024-04-07 23:52:49 +0200

    Change base route and add 2 new routes

--- src/const/routes.ts
+++ src/const/routes.ts
@@ -1,7 +1,9 @@
-const BASE_ROUTE = "https://note-backend-og5k.onrender.com";
+const BASE_ROUTE = "https://note-app-mr1c.onrender.com";
 
 export enum ROUTES {
   GET_ALL = BASE_ROUTE + "/notes",
   CREATE = BASE_ROUTE + "/note/create",
   DELETE = BASE_ROUTE + "/note/",
+  TOGGLE_PIN = BASE_ROUTE + "/note/togglePin/",
+  UPDATE_NOTE = BASE_ROUTE + "/note/update",
 }
