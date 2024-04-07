31ceb4a0b14ca152d13e2bbda6e3abf577b2474f
toNy5oo <lindirizzoemail@gmail.com>
2024-04-07 23:53:57 +0200

    Change text

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
     <div className="w-full h-full flex justify-center items-center">
-    <h2 className="animate-spin">🌀</h2><h2>Please have patience while the API server spins up...</h2>
+    <h2 className="animate-spin">🌀</h2><h2>Please have patience while the (free) server instance spins up...</h2>
     </div>)
 }
