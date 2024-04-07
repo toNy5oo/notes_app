c10fd5cf38a19cec0a11efc85615e13f96fcf827
toNy5oo <lindirizzoemail@gmail.com>
2024-04-07 23:51:43 +0200

    Move styles of containers

--- src/const/styles.ts
+++ src/const/styles.ts
@@ -1,5 +1,18 @@
 export const pinStyle = {
-        size: 40,
-        color: "white",
-        className: "cursor-pointer bg-slate-800 p-3 rounded-full relative -top-3 left-7"
+        size: 24,
+        color: "black",
+        className: "cursor-pointer bg-gray-100 p-1 rounded-full "
       }
+
+  export const cardStyles = `bg-white w-[250px] min-h-[250px] max-h-[400px] pb-3 pt-2 hover:shadow-lg border p-3 rounded-md border-t-thin`
+  export const listStyles = `bg-white w-[50%] min-h-[200px] max-h-[400px] mx-auto border rounded-md p-4 hover:shadow-lg border-l-thin cursor-pointer`
+
+  export const iconProps = {
+    color: 'grey',
+    size: 16
+}
+
+export const spinnerProps = {
+  color: 'black',
+  size: 14
+}
