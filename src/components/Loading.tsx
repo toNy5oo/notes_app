473634992a5c963f490b34c2d98f9c8210489d39
toNy5oo <lindirizzoemail@gmail.com>
2024-04-10 23:07:09 +0200

    Remove default export

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,4 @@
-export default function Loading() {
+export function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="text-center"> 
