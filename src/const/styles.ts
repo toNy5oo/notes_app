544b42c4cd4f927b9a1692bdf8e9cecf7f6edd8c
toNy5oo <lindirizzoemail@gmail.com>
2024-04-10 17:26:12 +0200

    Change layout style to dynamically apply height

--- src/const/styles.ts
+++ src/const/styles.ts
@@ -4,15 +4,21 @@ export const pinStyle = {
         className: "cursor-pointer bg-gray-100 p-1 rounded-full "
       }
 
-  export const cardStyles = `bg-white w-[250px] min-h-[250px] max-h-[400px] pb-3 pt-2 hover:shadow-lg border p-3 rounded-md border-t-thin`
-  export const listStyles = `bg-white w-[50%] min-h-[200px] max-h-[400px] mx-auto border rounded-md p-4 hover:shadow-lg border-l-thin cursor-pointer`
-
-  export const iconProps = {
-    color: 'grey',
-    size: 16
-}
-
+export const iconProps = {
+        color: 'grey',
+        size: 16,
+        cursor: 'pointer'
+      }
+      
 export const spinnerProps = {
-  color: 'black',
-  size: 14
-}
+        color: 'black',
+        size: 14
+      }
+
+export const gridContainerStyle = `xs:columns-1 sm:columns-2 md:columns-3 lg:columns-4`; 
+export const listContainerStyle = `flex flex-col xs:w-full sm:w-[75%] md:w-[55%] lg:w-[40%] mx-auto`; 
+
+export const noteStyle = `m-2 p-4 -pt-2 pb-6 border rounded-md hover:shadow-lg transition-shadow duration-200 ease-in-out bg-white overflow-hidden`;
+
+export const cardStyles = `w-[250px] md:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1rem)] xl:w-[calc(25%-1rem)] p-3 border rounded-md hover:shadow-lg transition-shadow duration-200 ease-in-out bg-white`;
+export const listStyles = `w-full p-4 border rounded-md hover:shadow-lg transition-shadow duration-200 ease-in-out bg-white`;
