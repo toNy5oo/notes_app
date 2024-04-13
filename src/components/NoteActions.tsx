19bf9146adc33f6f468e10c75876ad91803dda1d
toNy5oo <lindirizzoemail@gmail.com>
2024-04-13 23:37:43 +0200

    Remove unused style

--- src/components/NoteActions.tsx
+++ src/components/NoteActions.tsx
@@ -1,6 +1,7 @@
 import { INote } from '@/interface/notes_interface';
 import { PinAction } from './PinAction';
 import { useState } from 'react';
+import { Separator } from './ui/separator';
 import { ChangeColorAction } from './ChangeColorAction';
 import { DeleteNoteAction } from './DeleteNoteAction';
 
@@ -13,13 +14,15 @@ export function NoteActions({ note }: NoteActionsProps) {
   const [showColorPicker, setShowColorPicker] = useState(false);
 
    return (
-    <div className="absolute bottom-0 left-0 right-0 bg-opacity-80 bg-white z-10 flex justify-around p-2 animate-slideAndFadeFromBottom">
+    <div className="absolute bottom-0 left-0 right-0 bg-opacity-80 bg-gray-500 z-10 flex justify-around p-2 animate-slideAndFadeFromBottom">
       {showColorPicker ? (
         <ChangeColorAction note={note} onToggle={() => setShowColorPicker(false)} showColorPicker={showColorPicker}/>
       ) : (
-        <div className="flex justify-around w-full">
+        <div className="flex justify-evenly w-full">
           <PinAction note={note} />
-          <ChangeColorAction note={note} onToggle={() => setShowColorPicker(true)} showColorPicker={showColorPicker}/>
+              <Separator orientation='vertical' />
+          <ChangeColorAction note={note} onToggle={() => setShowColorPicker(!showColorPicker)} showColorPicker={showColorPicker}/>
+              <Separator orientation='vertical' />
           <DeleteNoteAction note={note} />
         </div>
        )}
